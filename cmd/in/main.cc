26d3ca4a574ecf74c81ba54f4781fa101ec1056f
lbhdc <lbhdc@users.noreply.github.com>
2023-02-25 19:54:41 -0500

    refactor: move version string to library

--- cmd/in/main.cc
+++ cmd/in/main.cc
@@ -1,14 +1,13 @@
 #include "ichneutae/flag/parser.h"
+#include "ichneutae/version.h"
 #include <iostream>
-
-constexpr auto in_version = "0.0.0";
 
 const auto version = in::flag::boolean("version", false, "Print the program version");
 
 int main(int argc, char* argv[]) {
 	in::flag::parse(argc, argv);
 	if (*version) {
-		std::cout << in::flag::executable_name() << ' ' << in_version << '\n';
+		std::cout << in::flag::executable_name() << ' ' << in::version << '\n';
 		std::exit(EXIT_SUCCESS);
 	}
 	std::cout << "works\n";
---
+++ ichneutae/version.h
@@ -0,0 +1,8 @@
+#ifndef ICHNEUTAE_VERSION_H
+#define ICHNEUTAE_VERSION_H
+
+namespace in {
+constexpr auto version = "0.0.0";
+}
+
+#endif
