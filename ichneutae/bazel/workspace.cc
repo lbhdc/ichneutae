2252af91e4cbb37b427ff509c1b9418286c78ff8
lbhdc <lbhdc@users.noreply.github.com>
2023-02-26 22:19:02 -0500

    fix: lifetime bug change string_view to string

--- ichneutae/bazel/workspace.cc
+++ ichneutae/bazel/workspace.cc
@@ -1,10 +1,11 @@
 #include "ichneutae/bazel/workspace.h"
 #include "ichneutae/os/path.h"
 #include <filesystem>
+#include <iostream>
 
 namespace in::bazel {
 result_or<std::string> workspace(std::string_view current_dir) noexcept {
-	auto cwd = current_dir;
+	auto cwd = std::string(current_dir);
 	if (cwd.empty()) {
 		cwd = std::filesystem::current_path().string();
 	}
@@ -12,12 +13,16 @@ result_or<std::string> workspace(std::string_view current_dir) noexcept {
 	while (not cwd.empty()) {
 		std::string path;
 		// loop through dir looking for WORKSPACE file
-		for (const auto& entry : std::filesystem::directory_iterator(cwd)) {
-			path = entry.path();
-			if ("WORKSPACE" == os::file_name(path)) {
-				path = os::dir(path);
-				return path;
+		try {
+			for (const auto& entry : std::filesystem::directory_iterator(cwd)) {
+				path = entry.path();
+				if ("WORKSPACE" == os::file_name(path)) {
+					path = os::dir(path);
+					return path;
+				}
 			}
+		} catch (std::filesystem::filesystem_error& e) {
+			return failed_precondition("bazel: " + std::string(e.what()) + " cwd=" + std::string(cwd));
 		}
 		// walk to parent directory
 		cwd = in::os::dir(cwd);
